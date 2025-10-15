fd7639f0b9f8db59aed6c6db65a0a432bfdd9518
DaV1tch <38894706+DaVitch@users.noreply.github.com>
2025-10-15 10:07:51 +0900

    Delete out directory

--- out/2024_8638_03.js
+++
@@ -1,8 +0,0 @@
-function f() {}
-const wasm_code = new Uint8Array([0,97,115,109,1,0,0,0,1,4,1,96,0,0,2,13,1,7,105,109,112,111,114,116,115,1,102,0,0,7,9,1,5,114,101,101,120,112,0,0]);
-const wasm_module = new WebAssembly.Module(wasm_code);
-const instance = new WebAssembly.Instance(wasm_module, { imports: { f: f } });
-const reexp = instance.exports.reexp;
-for (let i = 0; i < 20000; i++) {
-    reexp();
-}
